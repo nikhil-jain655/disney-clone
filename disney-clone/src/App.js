b4ce0e891fb19a15fd01262dc5e3ea8b2118fdd5
Nikhil Jain <nik.jainpara@gmail.com>
2024-03-12 21:54:39 +0530

    Main window implementation

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from  'react-router-dom'
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      test
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
